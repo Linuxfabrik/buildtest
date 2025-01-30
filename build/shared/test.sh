47fdd1e8fc2272eeb1a3da24cead6a03c9f49334
Markus Frei <markus.frei@linuxfabrik.ch>
2025-01-30 16:33:36 +0100

    .

--- build/shared/test.sh
+++ build/shared/test.sh
@@ -1,1 +1,8 @@
 echo "Linuxfabrik $(uname -a)"
+for dir in check-plugins/*; do
+  check=$(basename $dir)
+  if [ -e $dir/.windows ]; then
+      echo $check >> /tmp/windows-checks
+      echo "'$check'," >> /tmp/windows-checks-ps-list
+  fi
+done
