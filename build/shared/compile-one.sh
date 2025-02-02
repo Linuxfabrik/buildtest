4bfbe7232caf0757fab6cc4e1f7466276e7823a1
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-02 17:54:26 +0100

    .

--- build/shared/compile-one.sh
+++ build/shared/compile-one.sh
@@ -6,9 +6,8 @@ PLUGIN_DIR=$1
 PLUGIN_NAME=$2
 OUTPUT_DIR=$3
 
-PLUGIN="$2/$2"
-
 echo -e "\nCompiling $PLUGIN_NAME..."
+PLUGIN="$PLUGIN_NAME/$PLUGIN_NAME"
 python -m nuitka \
     --assume-yes-for-downloads \
     --output-dir=$OUTPUT_DIR/ \
