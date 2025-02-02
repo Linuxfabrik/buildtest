11b7fc59e82b3b1fabb4ca521d99141a605739a9
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-02 15:03:28 +0100

    .

---
+++ build/shared/all-get-latest-version.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_OWNER=$1
+REPO_NAME=$2
+GITHUB_TOKEN=$3
+
+GH_API_URL="https://api.github.com/repos/${{ github.repository_owner }}/${{ github.event.repository.name }}/actions/artifacts"
+ARTIFACT_NAME="version"
+
+echo "Fetching latest artifact..."
+
+# Fetch all artifacts and filter by name
+ARTIFACT_INFO=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+-H "Accept: application/vnd.github.v3+json" "$GH_API_URL" | \
+jq -r ".artifacts | map(select(.name == \"$ARTIFACT_NAME\")) | sort_by(.created_at) | last // {}")
+
+# Extract the download URL
+ARTIFACT_URL=$(echo "$ARTIFACT_INFO" | jq -r ".archive_download_url // empty")
+
+if [[ -z "$ARTIFACT_URL" ]]; then
+    echo "❌ No previous version artifact found. Exiting..."
+    exit 1
+else
+    echo "✅ Downloading artifact from: $ARTIFACT_URL"
+    curl -L -o version.zip -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "$ARTIFACT_URL"
+    mkdir -p version_artifact
+    unzip -o version.zip -d version_artifact
+    VERSION=$(cat version_artifact/version.txt)
+    echo "✅ Extracted version: $VERSION"
+    echo "VERSION=$VERSION" >> $GITHUB_ENV
+fi
---
+++ build/shared/windows-create-wxs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+WXS_FILE="$1"
+PACKAGE_VERSION=$2
+
+cat <<EOF > $WXS_FILE
+<Wix xmlns="http://wixtoolset.org/schemas/v4/wxs">
+  <Package
+      Name="Linuxfabrik Monitoring Plugins"
+      Version="$PACKAGE_VERSION"
+      Manufacturer="Linuxfabrik GmbH, Zurich, Switzerland"
+      UpgradeCode="bb340ae1-12a5-41d3-a27f-8677df3bdb2b"
+  >
+      <StandardDirectory Id="CommonAppDataFolder">
+          <Directory Id="Icinga2Dir" Name="icinga2">
+              <Directory Id="UsrDir" Name="usr">
+                  <Directory Id="Lib64Dir" Name="lib64">
+                      <Directory Id="NagiosDir" Name="nagios">
+                          <Directory Id="PluginsDir" Name="plugins">
+                              <!-- Automatically includes all files from the specified directory -->
+                              <Files Include="C:\tmp\output\summary\check-plugins\**" />
+                          </Directory>
+                      </Directory>
+                  </Directory>
+              </Directory>
+          </Directory>
+      </StandardDirectory>
+  </Package>
+</Wix>
+EOF
