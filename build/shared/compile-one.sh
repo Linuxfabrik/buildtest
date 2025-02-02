95b85b5e3f5bc88e179074b0d395190cdc84bcd1
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-02 19:34:47 +0100

    Update compile-one.sh

--- build/shared/compile-one.sh
+++ build/shared/compile-one.sh
@@ -6,14 +6,32 @@ PLUGIN_DIR=$1
 PLUGIN_NAME=$2
 OUTPUT_DIR=$3
 
-echo -e "\nCompiling $PLUGIN_NAME..."
+if [ "$PLUGIN_NAME" == "example" ]; then
+    echo "Ignoring 'example' plugin"
+    exit 0
+fi
+
+echo "Compiling $PLUGIN_NAME..."
 PLUGIN="$PLUGIN_NAME/$PLUGIN_NAME"
-python -m nuitka \
-    --assume-yes-for-downloads \
-    --output-dir=$OUTPUT_DIR/ \
-    --remove-output \
-    --standalone \
-    "$PLUGIN_DIR/$PLUGIN"
+
+if uname -a | grep -q "_NT"; then
+    # We are on Windows.
+    python -m nuitka \
+        --assume-yes-for-downloads \
+        --output-dir=$OUTPUT_DIR/ \
+        --remove-output \
+        --standalone \
+        --msvc=latest \
+        "$PLUGIN_DIR/$PLUGIN"
+else
+    python -m nuitka \
+        --assume-yes-for-downloads \
+        --output-dir=$OUTPUT_DIR/ \
+        --remove-output \
+        --standalone \
+        "$PLUGIN_DIR/$PLUGIN"
+fi
+
 if [[ -e "$OUTPUT_DIR/$PLUGIN_NAME.dist/$PLUGIN_NAME.bin" ]]; then
     # On Linux, files have the ".bin" extension.
     # On Windows, files are already named ".exe".
