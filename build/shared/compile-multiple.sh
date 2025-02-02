9b281dd4a1c7106f3ed7c8d42234fe4a0b8becde
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-02 18:00:29 +0100

    Update compile-multiple.sh

--- build/shared/compile-multiple.sh
+++ build/shared/compile-multiple.sh
@@ -26,7 +26,7 @@ for plugin in "${plugins[@]}"; do
     fi
     echo "Processing plugin: $plugin"
     if [[ -d "$PLUGIN_DIR/$plugin" ]]; then
-        echo pwd
+        echo $(pwd)
         ./compile-one.sh "$PLUGIN_DIR" "$plugin" "$COMPILE_DIR"
     else
         echo "Directory $PLUGIN_DIR/$plugin does not exist. Skipping."
