938453b6ee1a7d46f2e7d5f516c4a3aeea85cbe5
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-03 07:56:47 +0100

    .

--- build/shared/create-wxs.sh
+++ build/shared/create-wxs.sh
@@ -21,7 +21,7 @@ cat <<EOF > $WXS_FILE
                       <Directory Id="NagiosDir" Name="nagios">
                           <Directory Id="PluginsDir" Name="plugins">
                               <!-- Automatically includes all files from the specified directory -->
-                              <Files Include="$DIST_FOLDER\**" />
+                              <Files Include="**" />
                           </Directory>
                       </Directory>
                   </Directory>
