5d9b00f7917221af064a89b9cc7b5a7328a66161
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-11 19:26:14 +0100

    .

--- build/merge-to-dist.sh
+++ build/merge-to-dist.sh
@@ -3,13 +3,23 @@
 
 set -e -x
 
-for LFMP_TARGET_DISTRO in $LFMP_TARGET_DISTROS; do
-    for PLUGINS in check-plugins notification-plugins event-plugins; do
-        if [[ -d $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS && -n "$(ls -A $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS)" ]]; then
-            mkdir -p $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS
-            # directory exists and is not empty
-            echo "cp --archive $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS/*.dist/. $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS/"
-            \cp --archive $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS/*.dist/. $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS/
-        fi
+if uname -a | grep -q "_NT"; then
+    # We are on Windows.
+    mkdir -p "$LFMP_DIR_DIST"
+    cd "$LFMP_DIR_COMPILED"
+    for dir in */*; do
+        \cp --recursive --verbose "$dir"* "$LFMP_DIR_DIST"/
     done
-done
+else
+    # We are on the Ubuntu VM (not in the container).
+    for LFMP_TARGET_DISTRO in $LFMP_TARGET_DISTROS; do
+        for PLUGINS in check-plugins notification-plugins event-plugins; do
+            if [[ -d $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS && -n "$(ls -A $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS)" ]]; then
+                mkdir -p $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS
+                # directory exists and is not empty
+                echo "cp --archive $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS/*.dist/. $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS/"
+                \cp --archive $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS/*.dist/. $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS/
+            fi
+        done
+    done
+fi
