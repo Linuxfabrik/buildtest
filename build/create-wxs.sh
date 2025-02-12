731746131fcec3c3b0ba9fb8b81846040adfbae8
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-12 17:33:44 +0100

    Update create-wxs.sh

--- build/create-wxs.sh
+++ build/create-wxs.sh
@@ -4,51 +4,38 @@ set -e -x
 
 cat > "$LFMP_DIR_PACKAGED/lfmp.wxs" << EOF
 <Wix xmlns="http://wixtoolset.org/schemas/v4/wxs">
-  <Product
-      Manufacturer="Linuxfabrik GmbH"
-      Name="Linuxfabrik Monitoring Plugins"
-      UpgradeCode="bb340ae1-12a5-41d3-a27f-8677df3b8032"
-      Version="$LFMP_VERSION">
-      
-    <!-- Package: Only package-level settings go here -->
-    <Package>
-      <MediaTemplate EmbedCab="yes"/>
-    </Package>
+    <Package
+        Name="Linuxfabrik Monitoring Plugins"
+        Version="$LFMP_VERSION"
+        Manufacturer="Linuxfabrik GmbH"
+        UpgradeCode="{bb340ae1-12a5-41d3-a27f-8677df3b8032}">
 
-    <!-- The directory tree must be defined under Product, typically starting with TARGETDIR -->
-    <Directory Id="TARGETDIR" Name="SourceDir">
-      <Directory Id="CommonAppDataFolder">
-        <Directory Id="Icinga2Dir" Name="icinga2">
-          <Directory Id="UsrDir" Name="usr">
-            <Directory Id="Lib64Dir" Name="lib64">
-              <Directory Id="NagiosDir" Name="nagios">
-                <Directory Id="PluginsDir" Name="plugins">
-                  <!-- Define the component within the directory where its files (or actions) reside -->
-                  <Component Id="PluginComponent" Guid="*">
-                    <!-- Automatically include all files from the specified directory -->
-                    <Files Include="$LFMP_DIR_DIST\**" />
-                    <!-- Service control needs to be placed in a component -->
-                    <ServiceControl
-                        Id="icinga2"
-                        Name="Icinga 2"
-                        Start="both"
-                        Stop="both"
-                        Wait="yes"/>
-                  </Component>
+        <MediaTemplate EmbedCab="yes" />
+
+        <StandardDirectory Id="CommonAppDataFolder">
+            <Directory Id="Icinga2Dir" Name="icinga2">
+                <Directory Id="UsrDir" Name="usr">
+                    <Directory Id="Lib64Dir" Name="lib64">
+                        <Directory Id="NagiosDir" Name="nagios">
+                            <Directory Id="PluginsDir" Name="plugins">
+                                <!-- Automatically includes all files from the specified directory -->
+                                <Files Include="$LFMP_DIR_DIST\**" />
+                                <Component Id="Icinga2ServiceControl" Guid="{7e398e63-b894-47d1-9375-eea744988032}">
+                                    <ServiceControl
+                                        Id="icinga2"
+                                        Name="Icinga 2"
+                                        Start="both"
+                                        Stop="both"
+                                        Wait="yes"/>
+                                </Component>
+                            </Directory>
+                        </Directory>
+                    </Directory>
                 </Directory>
-              </Directory>
             </Directory>
-          </Directory>
-        </Directory>
-      </Directory>
-    </Directory>
+        </StandardDirectory>
 
-    <!-- Feature: Reference the component so it gets installed -->
-    <Feature Id="MainFeature" Title="Linuxfabrik Monitoring Plugins" Level="1">
-      <ComponentRef Id="PluginComponent"/>
-    </Feature>
-
-  </Product>
+    </Package>
 </Wix>
 EOF
 echo $(cat "$LFMP_DIR_PACKAGED/lfmp.wxs")
@@ -58,3 +45,10 @@ echo $(cat "$LFMP_DIR_PACKAGED/lfmp.wxs")
 # * https://docs.firegiant.com/wix3/xsd/wix/package/
 # * https://docs.firegiant.com/wix3/xsd/wix/component/
 # and other.
+
+# WIX1148: Invalid MSI package version: '2025022901'. The Windows Installer SDK says that MSI
+# package versions must have a major version less than 256, a minor version less than 256,
+# and a build version less than 65536. The revision value is ignored but version labels and
+# metadata are not allowed. Violating the MSI rules sometimes works as expected but the
+# behavior is unpredictable and undefined. Future versions of WiX might treat invalid package
+# versions as an error.
