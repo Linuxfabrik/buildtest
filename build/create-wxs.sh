c2ca84308bbbd6a57e314b06614ad5d88a733b09
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-12 16:16:44 +0100

    Update create-wxs.sh

--- build/create-wxs.sh
+++ build/create-wxs.sh
@@ -4,43 +4,57 @@ set -e -x
 
 cat > "$LFMP_DIR_PACKAGED/lfmp.wxs" << EOF
 <Wix xmlns="http://wixtoolset.org/schemas/v4/wxs">
-    <Package
-        Name="Linuxfabrik Monitoring Plugins"
-        Version="$LFMP_VERSION"
-        Manufacturer="Linuxfabrik GmbH"
-        UpgradeCode="bb340ae1-12a5-41d3-a27f-8677df3b8032"
-    >
-
-        <!-- https://docs.firegiant.com/wix3/xsd/wix/mediatemplate/ -->
-        <MediaTemplate EmbedCab="yes"/>
-
-        <!-- https://docs.firegiant.com/wix3/xsd/wix/servicecontrol/ -->
-        <!-- Stop/Start Icinga-Agent on Install/Uninstall -->
-        <ServiceControl
-            Id="icinga2"
-            Name="Icinga 2"
-            Stop="both"
-            Start="both"
-            Wait="yes"
-        />
-
-        <!-- Deploy to C:\ProgramData\icinga2\usr\lib64\nagios\plugins -->
-        <StandardDirectory Id="CommonAppDataFolder">
-            <Directory Id="Icinga2Dir" Name="icinga2">
-                <Directory Id="UsrDir" Name="usr">
-                    <Directory Id="Lib64Dir" Name="lib64">
-                        <Directory Id="NagiosDir" Name="nagios">
-                            <Directory Id="PluginsDir" Name="plugins">
-                                <!-- Automatically include all files from the specified directory -->
-                                <Files Include="$LFMP_DIR_DIST\**" />
-                            </Directory>
-                        </Directory>
-                    </Directory>
-                </Directory>
-            </Directory>
-        </StandardDirectory>
-
+  <Product
+      Manufacturer="Linuxfabrik GmbH"
+      Name="Linuxfabrik Monitoring Plugins"
+      UpgradeCode="bb340ae1-12a5-41d3-a27f-8677df3b8032"
+      Version="$LFMP_VERSION">
+      
+    <!-- Package: Only package-level settings go here -->
+    <Package>
+      <MediaTemplate EmbedCab="yes"/>
     </Package>
+
+    <!-- The directory tree must be defined under Product, typically starting with TARGETDIR -->
+    <Directory Id="TARGETDIR" Name="SourceDir">
+      <Directory Id="CommonAppDataFolder">
+        <Directory Id="Icinga2Dir" Name="icinga2">
+          <Directory Id="UsrDir" Name="usr">
+            <Directory Id="Lib64Dir" Name="lib64">
+              <Directory Id="NagiosDir" Name="nagios">
+                <Directory Id="PluginsDir" Name="plugins">
+                  <!-- Define the component within the directory where its files (or actions) reside -->
+                  <Component Id="PluginComponent" Guid="*">
+                    <!-- Automatically include all files from the specified directory -->
+                    <Files Include="$LFMP_DIR_DIST\**" />
+                    <!-- Service control needs to be placed in a component -->
+                    <ServiceControl
+                        Id="icinga2"
+                        Name="Icinga 2"
+                        Start="both"
+                        Stop="both"
+                        Wait="yes"/>
+                  </Component>
+                </Directory>
+              </Directory>
+            </Directory>
+          </Directory>
+        </Directory>
+      </Directory>
+    </Directory>
+
+    <!-- Feature: Reference the component so it gets installed -->
+    <Feature Id="MainFeature" Title="Linuxfabrik Monitoring Plugins" Level="1">
+      <ComponentRef Id="PluginComponent"/>
+    </Feature>
+
+  </Product>
 </Wix>
 EOF
 echo $(cat "$LFMP_DIR_PACKAGED/lfmp.wxs")
+
+# See
+# * https://docs.firegiant.com/wix3/xsd/wix/product/
+# * https://docs.firegiant.com/wix3/xsd/wix/package/
+# * https://docs.firegiant.com/wix3/xsd/wix/component/
+# and other.
