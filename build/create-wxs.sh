8dad42eac3068929778f74e789fb3a9ce3943eb2
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-12 15:20:01 +0100

    .

--- build/create-wxs.sh
+++ build/create-wxs.sh
@@ -10,14 +10,28 @@ cat > "$LFMP_DIR_PACKAGED/lfmp.wxs" << EOF
       Manufacturer="Linuxfabrik GmbH"
       UpgradeCode="bb340ae1-12a5-41d3-a27f-8677df3b8032"
   >
+
+      <!-- https://docs.firegiant.com/wix3/xsd/wix/mediatemplate/ -->
       <MediaTemplate EmbedCab="yes" />
+
+      <!-- https://docs.firegiant.com/wix3/xsd/wix/servicecontrol/ -->
+      <!-- Stop/Start Icinga-Agent on Install/Uninstall
+      <ServiceControl
+          Id="icinga2"
+          Name="Icinga 2"
+          Stop="both"
+          Start="both"
+          Wait="yes"
+      />
+
+      <!-- Deploy to C:\ProgramData\icinga2\usr\lib64\nagios\plugins -->
       <StandardDirectory Id="CommonAppDataFolder">
           <Directory Id="Icinga2Dir" Name="icinga2">
               <Directory Id="UsrDir" Name="usr">
                   <Directory Id="Lib64Dir" Name="lib64">
                       <Directory Id="NagiosDir" Name="nagios">
                           <Directory Id="PluginsDir" Name="plugins">
-                              <!-- Automatically includes all files from the specified directory -->
+                              <!-- Automatically include all files from the specified directory -->
                               <Files Include="$LFMP_DIR_DIST\**" />
                           </Directory>
                       </Directory>
@@ -25,6 +39,7 @@ cat > "$LFMP_DIR_PACKAGED/lfmp.wxs" << EOF
               </Directory>
           </Directory>
       </StandardDirectory>
+
   </Package>
 </Wix>
 EOF
--- build/merge-to-dist.sh
+++ build/merge-to-dist.sh
@@ -14,7 +14,7 @@ else
     # We are on the Ubuntu VM (not in the container).
     for LFMP_TARGET_DISTRO in $LFMP_TARGET_DISTROS; do
         for PLUGINS in check-plugins notification-plugins event-plugins; do
-            if [[ -d $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS && -n "$(ls -A $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS)" ]]; then
+            if [[ -d $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS && -n "$(ls --almost-all $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS)" ]]; then
                 mkdir -p $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS
                 # directory exists and is not empty
                 echo "cp --archive $LFMP_DIR_COMPILED/$LFMP_TARGET_DISTRO/$PLUGINS/*.dist/. $LFMP_DIR_DIST/$LFMP_TARGET_DISTRO/$PLUGINS/"
