4ca968ee022bcf013df7f5ba4c7d71913b9d16cf
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-15 14:18:54 +0100

    Update merge-to-dist.sh

--- build/merge-to-dist.sh
+++ build/merge-to-dist.sh
@@ -6,7 +6,7 @@ set -e -x
 if uname -a | grep -q "_NT"; then
     mkdir -p $LFMP_DIR_DIST
     for d in $LFMP_DIR_COMPILED/$PLUGINS/*.dist; do
-        cp --recursive "$d"/* $LFMP_DIR_DIST/; done
+        cp --recursive "$d"/* $LFMP_DIR_DIST/
     done
 else
     # We are on the Ubuntu VM (not in the container).
