e4616cdb44300db2fe3c6436adf3f372fb8e865d
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-15 15:20:40 +0100

    Update merge-to-dist.sh

--- build/merge-to-dist.sh
+++ build/merge-to-dist.sh
@@ -5,6 +5,7 @@ set -e -x
 
 if uname -a | grep -q "_NT"; then
     mkdir -p $LFMP_DIR_DIST
+    echo "\cp --archive $LFMP_DIR_COMPILED/check-plugins/*.dist/. $LFMP_DIR_DIST/"
     \cp --archive $LFMP_DIR_COMPILED/check-plugins/*.dist/. $LFMP_DIR_DIST/
 else
     # We are on the Ubuntu VM (not in the container).
